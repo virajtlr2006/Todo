82439e347d79fc83d39c4b58bd1802f67266b9c8
Viraj Tailor <virajtlr2006@gmail.com>
2025-07-18 15:38:22 +0530

    all routes created

--- index.js
+++ index.js
@@ -5,6 +5,29 @@ const app = express()
 app.listen(5230,()=>{
     console.log("App started and listening on port 8080.")
 })
-app.get("/yash",async (req,res) => {
+app.get("/",async (req,res) => {
     res.send("Response sended to home");
 })
+
+// all todo route
+
+app.get("/todo",async (req,res) => {
+    res.send("All todos");
+})
+
+// new todo
+app.post("/todo/new",async (req,res) => {
+    res.send("New todo");
+})
+
+//  single todo with  id
+
+//Delete todo
+app.get("/todo/delete",async (req,res) => {
+    res.send("Delete todo");
+})
+
+app.get("/todo/:id",async (req,res) => {
+    res.send("Single todo");
+})
+
